e2beed54651bd98f15e1a88a844527cc4975f956
Pranav Gupta <146617022+Pranav-7302@users.noreply.github.com>
2025-02-16 12:05:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-let submitBtn = document.getElementById('button')
 let fname = document.querySelector('.fname')
 let lname = document.querySelector('.lname')
-function getFormvalue(){
-  alert(`${fname.value} ${lname.value}`)
-}
 
-submitBtn.addEventListener('click',()=>{
-  getFormvalue()
-})
+
+function getFormvalue() {
+
+  //Write your code here
+ 
+  alert(`${fname.value} ${lname.value}`)
+
+}
